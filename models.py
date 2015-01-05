8a51370ebe49253c3f5a7762fea1115fe8d7179c
DWu39 <dwu399@gmail.com>
2015-01-05 16:57:46 -0500

    added modal and fixed weird z-index bug

--- models.py
+++ models.py
@@ -8,6 +8,9 @@ class Exercise(db.Model):
   name = db.Column(db.String(40), unique=True)
   exercise_type = db.Column(db.Boolean()) # True for strength, False for cardio
 
+  equipment_id = db.Column(db.Integer, db.ForeignKey('equipment.id'))
+  equipment = db.relationship('Equipment', backref='exercises', lazy='dynamic')
+
   def __init__(self, name, exercise_type):
     self.name = name
     self.exercise_type = exercise_type
@@ -15,3 +18,16 @@ class Exercise(db.Model):
   def __repr__(self):
     string_of_type = 'Strength' if self.exercise_type else 'Cardio'
     return "%s, %s" %(self.name, string_of_type)
+
+
+class Equipment(db.Model):
+  __tablename__ = 'equipment'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(40), unique=True)
+
+  def __init__(self, name):
+    self.name = name
+
+  def __repr__(self):
+    return self.name
